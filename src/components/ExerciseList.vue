aa3a9cf9be724f40e73d22f8bcf9c579f6fe9741
Alar Laanep <alaanep@gmail.com>
2022-09-19 20:32:04 +0300

    Vaikimisi kujundus eemaldamine

--- src/components/ExerciseList.vue
+++ src/components/ExerciseList.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <h1>{{title}}</h1>
+    <div class="text-center bg-gray-50 text-dark-300">
+        <h1 class="font-bold">{{title}}</h1>
         <ul>
             <li>Harjutus 1: Kirjeldus</li>
             <li>Harjutus 2: Kirjeldus</li>
