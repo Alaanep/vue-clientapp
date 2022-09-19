79b4000d31df1ae22d40d53f632d5e3f216b7423
Alar Laanep <alaanep@gmail.com>
2022-09-19 20:38:42 +0300

    Harjutuste nimekiri lokaalsest muutujast

--- src/components/ExerciseList.vue
+++ src/components/ExerciseList.vue
@@ -2,10 +2,9 @@
     <div class="text-center bg-gray-50 text-dark-300">
         <h1 class="font-bold">{{title}}</h1>
         <ul>
-            <li>Harjutus 1: Kirjeldus</li>
-            <li>Harjutus 2: Kirjeldus</li>
-            <li>Harjutus 3: Kirjeldus</li>
-            <li>Harjutus 4: Kirjeldus</li>
+            <li v-for="item in localExerciseList" :key="item.name">
+                {{item.name}} : {{item.description}}
+            </li>
         </ul>
     </div>
 </template>
@@ -13,4 +12,12 @@
 <script setup lang="ts">
 defineProps<{title: string}>();
 //import { title } from 'process';
+
+const localExerciseList=[
+    {name: 'Harjutus 1', description: 'Harjutus 1'},
+    {name: 'Harjutus 2', description: 'Harjutus 2'},
+    {name: 'Harjutus 3', description: 'Harjutus 3'},
+    {name: 'Harjutus 4', description: 'Harjutus 4'},
+];
+
 </script>
