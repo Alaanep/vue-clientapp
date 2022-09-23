6ee5810507a4978e528611ba51959a21f0b181d2
Alar Laanep <alaanep@gmail.com>
2022-09-23 08:46:22 +0300

    ndl 4 valmis

--- src/components/ExerciseList.vue
+++ src/components/ExerciseList.vue
@@ -2,25 +2,25 @@
     <div class="bg-gray-50 py-12 px-4 sm:px-6 lg:px-8 text-dark-300">
         <div class="text-center bg-gray-50">
             <h1 class="font-bold">{{ title }}</h1>
-            <DataTable :value="exercises">
+            <div v-if="exercises.length===0">Harjutused puuduvad</div>
+            <DataTable :value="exercises" v-if="exercises.length>0">
                 <Column field="name" header="Nimi" />
                 <Column field="description" header="Kirjeldus" />
             </DataTable>
         </div>
     </div>
 </template>
-
 <script setup lang="ts">
-
+    
 import {  } from 'process';
 //import useExercise from '@/stores/ExercisesStore';
 import {useExercisesStore} from '@/stores/ExercisesStore';
 //import { onMounted } from 'vue';
 //import Exercise from './Exercise.vue';
-
 defineProps<{title: string}>();
 //const { exercises, load } = useExercise();
 //onMounted(() => load());
 const {exercises}=useExercisesStore();
+console.log(exercises.length);
 
 </script>
--- src/stores/ExercisesStore.ts
+++ src/stores/ExercisesStore.ts
@@ -4,11 +4,9 @@ import {Exercise} from '@/model/exercise';
 import ExercisesVue from '@/views/Exercises.vue';
 import {defineStore} from 'pinia';
 import {ref} from 'vue';
-
 /*const state = reactive<State>({
     exercises: [],
 });
-  
  let initialized=false;
 export default function useExercise(){
     const load=()=>{
@@ -22,22 +20,20 @@ export default function useExercise(){
             initialized=true;
         }
     };
-    
     const addExercise = (exercise: Exercise) => {
         state.exercises.push(exercise);
       };
-    
     return {...toRefs(state), load, addExercise};
-
 } */
-
 export const useExercisesStore = defineStore('exercisesStore', () => {
     const exercises = ref<Exercise[]>([
-      { name: 'Harjutus 1', description: 'Kirjeldus 1' },
-      { name: 'Harjutus 2', description: 'Kirjeldus 2' },
-      { name: 'Harjutus 3', description: 'Kirjeldus 3' },
-      { name: 'Harjutus 4', description: 'Kirjeldus 4' },
+      //{ name: 'Harjutus 1', description: 'Kirjeldus 1' },
+      //{ name: 'Harjutus 2', description: 'Kirjeldus 2' },
+      //{ name: 'Harjutus 3', description: 'Kirjeldus 3' },
+      //{ name: 'Harjutus 4', description: 'Kirjeldus 4' },
     ]);  
+
+
 
 const addExercise=(exercise: Exercise)=>{
     exercises.value.push(exercise);
